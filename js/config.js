e80025e6c1b84ca37aac99c664f913477492d286
sero31 <74045650+sero31@users.noreply.github.com>
2020-11-06 12:02:09 +0300

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,173 @@
+//       _           _    _                     _ 
+//      | |         | |  | |                   | |
+//   ___| | ___  ___| | _| |     ___   __ _  __| |
+//  / __| |/ _ \/ _ \ |/ / |    / _ \ / _` |/ _` |
+//  \__ \ |  __/  __/   <| |___| (_) | (_| | (_| |
+//  |___/_|\___|\___|_|\_\______\___/ \__,_|\__,_|
+//     version 2.16
+
+// URL for the server logo
+// Leave it blank to use text instead
+// (Put the files in the images folder)
+// (You can also use external URL)
+var l_serverImage = "logo.png";
+
+// Center logo?
+// 'true' or 'false'
+var l_centerLogo = false;
+
+// Display current map and gamemode?
+// 'true' or 'false'
+var l_displayMapGamemode = false;
+
+// Custom server name configuration
+// Leave it blank for automatic server name
+var l_serverName = "SoulRP";
+
+// Use a video for the background?
+// Otherwise it will use images
+// 'true' or 'false'
+var l_bgVideo = true;
+
+// Default available videos:
+// "aurora.webm"
+// "bluefall.webm"
+// "bluetilefloor.webm"
+// "bluewhitecircles.webm"
+// "bokehlens.webm"
+// "cleanbokeh.webm"
+// "coldnight.webm"
+// "colorfulburst.webm"
+// "dancingstars.webm"
+// "dropsflares.webm"
+// "focusorangebokeh.webm"
+// "glimmering.webm"
+// "glowingrings.webm"
+// "greencircles.webm"
+// "hues.webm"
+// "inthedeep.webm"
+// "modernflourish.webm"
+// "orangeaura.webm"
+// "warminside.webm"
+// "yellowgreen.webm"
+//
+// You can add your own video backgrounds
+// Just make sure they are using webm VP8 format 
+// (Place the videos in the backgrounds/videos folder)
+// (You can also use external URL)
+var l_background = "dropsflares.webm";
+
+// (Image-background only)
+// Enable map-based background image?
+// If enabled, the background will be an image based on the server current map
+// You need to have a background image with the same name as the map
+// Also, the image needs to be in .jpg format
+// E.g: gm_flatgrass.jpg, gm_construct.jpg, rp_downtown_v4c_v2.jpg
+// 'true' or 'false'
+var l_bgImageMapBased = false;
+
+// (Image-background only)
+// Place one or more images
+// Multiple images will automatically shuffle between them
+// (Place the files in the backgrounds/images folder)
+// (You can also use external URL)
+var l_bgImages = [
+	"touhou01.jpg",
+	"touhou02.jpg",
+	"touhou03.jpg"
+];
+
+// (Image-background only)
+// Random background images order?
+// 'true' or 'false'
+var l_bgImagesRandom = false;
+
+// (Image-background only)
+// Delay between background images changes
+// in milliseconds
+var l_bgImageDuration = 5000;
+
+// (Image-background only)
+// Image background fade velocity
+// in milliseconds
+var l_bgImageFadeVelocity = 2000;
+
+// Enable background overlay?
+// 'true' or 'false'
+var l_bgOverlay = true;
+
+// Background darkening amount
+// 0 for none (0%), 100 for pitch black (100%)
+var l_bgDarkening = 50;
+
+// Play music during the loading screen?
+// 'true' or 'false'
+var l_music = true;
+
+// Display current music name?
+// 'true' or 'false'
+var l_musicDisplay = true;
+
+// Music playlist
+// Add as many youtube ids/urls or ogg files you want
+// (Place the ogg files in the music folder)
+// (You can also use external URL for the ogg files)
+var l_musicPlaylist = [
+	{ogg: "HOME - Resonance.ogg", name: "HOME - Resonance"},
+];
+
+// Random music order?
+// 'true' or 'false'
+var l_musicRandom = true;
+
+// Music volume
+// Choose a value between 0 (quietest) and 100 (loudest)
+var l_musicVolume = 2.5;
+
+// Enable custom messages?
+// 'true' or 'false'
+var l_messagesEnabled = true;
+
+// Enter your custom messages below
+var l_messages = [
+	"Sometimes the one thing you need for growth is the one thing you are afraid to do",
+	"Life changes, so you must to",
+	"I see nothing, I know nothing! - Sgt. Schultz",
+	"Never too old, never too bad, never too late, never too sick to start from scratch once again, Like us.",
+	"If two wrongs dont make a right, Try three.",
+	"I am not lazy, i am on energy saving mode.",
+	"The more you weight the harder you are to kidnap. Stay safe, Eat cake.",
+	"An apple a day, keeps anyone away, if you throw it hard enough.",
+	"They say eat your greens, may as well smoke them to.",
+	"A year from now you will wish you had started today",
+	"Some people just need a high five. In the face. With a chair",
+	"people say doing nothing is impossible, I do nothing everyday.",
+	"Your life cant fall apart if you didnt have it together.",
+	"My friend thinks he's smart. He said onions are the only food that make you cry. So I threw a coconut at his face.",
+];
+
+// Random message order?
+// 'true' or 'false'
+var l_messagesRandom = true;
+
+// Delay between message changes
+// in milliseconds
+var l_messagesDelay = 7000;
+
+// Messages fade time
+// in milliseconds
+var l_messagesFade = 1000;
+
+// Show errors inside Garry's Mod?
+// Can be useful for finding errors
+// 'true' or 'false'
+var l_showErrorsIngame = true;
+
+//==============================================================
+//========================== WARNING ===========================
+//==============================================================
+//============ Do not edit anything below this line ============
+//==============================================================
+var checkConfigFile = function() {
+	return true;
+};
